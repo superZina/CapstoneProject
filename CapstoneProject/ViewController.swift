b26dd042eaed9a6163309cb25da57cd0d4937efe
이진하 <dkwlsfk2323@gmail.com>
2020-05-11 23:10:57 +0900

    Storyboard 생성

---
+++ CapstoneProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CapstoneProject
+//
+//  Created by 이진하 on 2020/05/11.
+//  Copyright © 2020 이진하. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
