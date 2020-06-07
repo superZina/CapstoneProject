cf68c790ebd890c1cda26f0b61b4fc58b5d9f496
이진하 <dkwlsfk2323@gmail.com>
2020-06-07 23:30:22 +0900

    지도 확대, 축소 버튼 추가

--- CapstoneProject/main/mainView.swift
+++ CapstoneProject/main/mainView.swift
@@ -11,16 +11,32 @@ import MapKit
 
 class mainView: UIViewController ,MKMapViewDelegate, CLLocationManagerDelegate{
     var userId:String = (UserDefaults.standard.value(forKey: "id") as? String)!
+    //회원정보 수정
     @IBAction func editUserInfo(_ sender: Any) {
         performSegue(withIdentifier: "edit", sender: nil)
     }
     
-    var manager = CLLocationManager()
-    var myLocations: [CLLocation] = []
+    //지도 확대, 축소 버튼
+    @IBAction func zoomMap(_ sender: Any) {
+        var region: MKCoordinateRegion = mainMap.region
+        region.span.latitudeDelta /= 2.0
+        region.span.longitudeDelta /= 2.0
+        mainMap.setRegion(region, animated: true)
+    }
+    @IBAction func aoomOutMap(_ sender: Any) {
+        var region: MKCoordinateRegion = mainMap.region
+        region.span.latitudeDelta = min(region.span.latitudeDelta * 2.0, 180.0)
+        region.span.longitudeDelta = min(region.span.longitudeDelta * 2.0, 180.0)
+        mainMap.setRegion(region, animated: true)
+    }
+    
+    
+    
     
     @IBOutlet weak var mainMap: MKMapView!
     @IBOutlet weak var location: UILabel!
-    
+    var manager = CLLocationManager()
+    var myLocations: [CLLocation] = []
     override func viewDidLoad() {
         var message = self.userId + "님 환영합니다!"
         super.viewDidLoad()
@@ -40,7 +56,14 @@ class mainView: UIViewController ,MKMapViewDelegate, CLLocationManagerDelegate{
         mainMap.mapType = MKMapType.standard
         mainMap.showsUserLocation = true
         
+        //메인뷰에 특정 위치 표시하기(정류소 위치 표시하기)
         setAnnotaion(lat: 37.450874, lon: 127.128890, delta: 1, title: "가천대학교", subtitle: "글로벌 캠퍼스")
+        setAnnotaion(lat: 37.450963, lon: 127.127133, delta: 1, title: "IT대학교", subtitle: "IT대 정류장")
+        setAnnotaion(lat: 37.451819, lon: 127.131554, delta: 1, title: "교육대학원", subtitle: "교육대학원앞 정류장")
+        setAnnotaion(lat: 37.452466, lon: 127.132906, delta: 1, title: "중앙도서관", subtitle: "중앙도서관 정류장")
+        setAnnotaion(lat: 37.453352, lon: 127.134065, delta: 1, title: "학생회관", subtitle: "학생회관 정류장")
+        setAnnotaion(lat: 37.456154, lon:  127.135095, delta: 1, title: "기숙사", subtitle: "기숙사 앞 정류장")
+    
     }
     @IBAction func myLocation(_ sender: Any) {
         
@@ -53,7 +76,7 @@ class mainView: UIViewController ,MKMapViewDelegate, CLLocationManagerDelegate{
         mainMap.setRegion(pRegion, animated: true)
         return pLocation
     }
-    
+    //지도에 특정 위치 표시
     func setAnnotaion(lat:CLLocationDegrees, lon:CLLocationDegrees , delta span :Double , title strTitle: String, subtitle strSubTitle :String){
         let annotaion = MKPointAnnotation()
         annotaion.coordinate = goLocation(latitudeValue: lat, longitudeValue: lon, delta: span)
@@ -84,12 +107,6 @@ class mainView: UIViewController ,MKMapViewDelegate, CLLocationManagerDelegate{
             
         })
         manager.stopUpdatingLocation()
-    }
-    func foLocation(lat:CLLocationDegrees,lon:CLLocationDegrees, delta span:Double) {
-        let pLocation = CLLocationCoordinate2DMake(lat, lon)
-        let spanValue = MKCoordinateSpan(latitudeDelta: span , longitudeDelta: span)
-        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
-        mainMap.setRegion(pRegion, animated: true)
     }
     
   
